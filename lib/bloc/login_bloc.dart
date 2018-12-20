14391790691c0946f5936f0b8654e683738deda8
Arnaud Phommasone <arnaud@agilityfeat.com>
2018-12-20 17:47:50 -0500

    Remove validators class in the login_bloc

--- lib/bloc/login_bloc.dart
+++ lib/bloc/login_bloc.dart
@@ -4,19 +4,6 @@ import 'package:trakref_app/repository/api_service.dart';
 import 'package:trakref_app/bloc/bloc_provider.dart';
 import 'package:trakref_app/models/logged_user_entity.dart';
 
-class Validators {
-  final validateEmail = StreamTransformer<String, String>.fromHandlers(
-      handleData: (email, sink) {
-        if (email.contains('@')) {
-          sink.add(email);
-        }
-        else {
-          sink.addError('Email is not valid');
-        }
-      }
-    );
-}
-
 class LoginBloc implements BlocBase {
   LoggedUser _user = LoggedUser.empty();
   LoginService _loginService = LoginService();
@@ -40,6 +27,7 @@ class LoginBloc implements BlocBase {
         LoggedUser user = LoggedUser.fromJson(result);
         print(user.toString());
         _user = user;
+        _goingNextScreenController.add(user);
     }).catchError((error) {
       _submitLoginController.addError(error);
         print('_loginService caught an error : $error');
@@ -50,5 +38,6 @@ class LoginBloc implements BlocBase {
   void dispose() {
     // TODO: implement dispose
     _submitLoginController.close();
+    _goingNextScreenController.close();
   }
 }
