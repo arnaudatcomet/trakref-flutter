8835a233900bf05b26b16bc368345c80a78ce5ec
Arnaud Phommasone <arnaud@agilityfeat.com>
2019-07-08 09:36:33 -0500

    Clean up login page (remove 'new' and add 'const' when possible)

--- lib/screens/login/page_login_bloc.dart
+++ lib/screens/login/page_login_bloc.dart
@@ -43,27 +43,27 @@ class _PageLoginBlocState extends State<PageLoginBloc> {
       onModelReady: (model) => model.clear(),
       builder: (context, model, child) {
         return Scaffold(
-            body: new Container(
-                padding: new EdgeInsets.all(20.0),
-                child: new Center(
-                  child: new Form(
+            body: Container(
+                padding: const EdgeInsets.all(20.0),
+                child: Center(
+                  child: Form(
                       key: _formKey,
-                      child: new ListView(
+                      child: ListView(
                         children: <Widget>[
-                          new Container(
-                            child: new Image.asset("assets/images/logo.png",
+                          Container(
+                            child: Image.asset("assets/images/logo.png",
                                 height: 50,
                                 alignment: Alignment.centerLeft,
                                 fit: BoxFit.fitHeight),
-                            margin: new EdgeInsets.only(top: 103, bottom: 30),
+                            margin: const EdgeInsets.only(top: 103, bottom: 30),
                           ),
-                          new Container(
-                            child: new Text("Welcome back.",
+                          Container(
+                            child: Text("Welcome back.",
                                 style: Theme.of(context).textTheme.title),
                           ),
-                          new Container(
-                              margin: new EdgeInsets.only(top: 30, bottom: 30),
-                              child: new TextFormField(
+                          Container(
+                              margin: const EdgeInsets.only(top: 30, bottom: 30),
+                              child: TextFormField(
                                 key: Key("UsernameKey"),
                                 validator: (value) {
                                   if (value.isEmpty) {
@@ -74,11 +74,11 @@ class _PageLoginBlocState extends State<PageLoginBloc> {
                                   this._data.username = value;
                                 },
                                 keyboardType: TextInputType.emailAddress,
-                                decoration: new InputDecoration(
+                                decoration: InputDecoration(
                                     hintText: 'Enter your username',
                                     labelText: 'Username'),
                               )),
-                          new TextFormField(
+                          TextFormField(
                             key: Key("PasswordKey"),
                             validator: (value) {
                               if (value.isEmpty) {
@@ -90,11 +90,11 @@ class _PageLoginBlocState extends State<PageLoginBloc> {
                             },
                             obscureText: (_showPassword == false),
                             textAlign: TextAlign.start,
-                            decoration: new InputDecoration(
+                            decoration: InputDecoration(
                                 labelText: 'Password',
-                                contentPadding: new EdgeInsets.all(0),
+                                contentPadding: const EdgeInsets.all(0),
                                 suffixIcon: Padding(
-                                    padding: EdgeInsets.only(right: 10),
+                                    padding: const EdgeInsets.only(right: 10),
                                     //loginBloc
                                     child: IconButton(
                                         icon: const Icon(Icons.remove_red_eye),
@@ -108,7 +108,7 @@ class _PageLoginBlocState extends State<PageLoginBloc> {
                               margin: EdgeInsets.only(top: 30),
                               child: ButtonTheme(
                                 height: 52.0,
-                                child: new RaisedButton(
+                                child: RaisedButton(
                                   key: Key('SubmitButton'),
                                   color: AppColors.blueTurquoise,
                                   child: (model.state == ViewState.Idle)
@@ -138,7 +138,7 @@ class _PageLoginBlocState extends State<PageLoginBloc> {
                                         print("Push to a next screen");
                                         _formKey.currentState.reset();
                                         Navigator.of(context).push(
-                                            new MaterialPageRoute(builder:
+                                            MaterialPageRoute(builder:
                                                 (BuildContext context) {
                                           return PageAccountsBloc(
                                               type: PageAccountsType.Home);
@@ -148,8 +148,8 @@ class _PageLoginBlocState extends State<PageLoginBloc> {
                                       }
                                     }
                                   },
-                                  shape: new RoundedRectangleBorder(
-                                    borderRadius: new BorderRadius.circular(4),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(4),
                                   ),
                                 ),
                               )),
@@ -168,7 +168,7 @@ class _PageLoginBlocState extends State<PageLoginBloc> {
                               LinkAppButton(
                                 onPressed: () {
                                   Navigator.of(context).push(
-                                      new MaterialPageRoute(
+                                       MaterialPageRoute(
                                           builder: (BuildContext context) {
                                     return PageResetPasswordBloc();
                                   }));
@@ -186,166 +186,4 @@ class _PageLoginBlocState extends State<PageLoginBloc> {
       },
     );
   }
-  /*
-  @override
-  Widget build(BuildContext context) {
-      return Scaffold(
-          body: new Container(
-              padding: new EdgeInsets.all(20.0),
-              child: new Center(
-                child: new Form(
-                    key: _formKey,
-                    child: new ListView(
-                      children: <Widget>[
-                        new Container(
-                          child: new Image.asset("assets/images/logo.png",
-                              height: 50,
-                              alignment: Alignment.centerLeft,
-                              fit: BoxFit.fitHeight),
-                          margin: new EdgeInsets.only(top: 103, bottom: 30),
-                        ),
-                        new Container(
-                          child: new Text("Welcome back.", style: Theme.of(context).textTheme.title
-                          ),
-                        ),
-                        new Container(
-                            margin: new EdgeInsets.only(top: 30, bottom: 30),
-                            child: new TextFormField(
-                              validator: (value) {
-                                if (value.isEmpty) {
-                                  return 'Please enter an username';
-                                }
-                              },
-                              onSaved: (String value) {
-                                this._data.username = value;
-                              },
-                              keyboardType: TextInputType.emailAddress,
-                              decoration: new InputDecoration(
-                                  hintText: 'Enter your username',
-                                  labelText: 'Username'
-                              ),
-                            )
-                        ),
-                        new TextFormField(
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return 'Please enter a password';
-                            }
-                          },
-                          onSaved: (String value) {
-                            this._data.password = value;
-                          },
-                          obscureText: (_showPassword == false),
-                          textAlign: TextAlign.start,
-                          decoration: new InputDecoration(
-                              labelText: 'Password',
-                              contentPadding: new EdgeInsets.all(0),
-                              suffixIcon: Padding(
-                                  padding: EdgeInsets.only(right: 10),
-                                  //loginBloc
-                                  child: IconButton(
-                                      icon: const Icon(Icons.remove_red_eye),
-                                      onPressed: () {
-                                        setState(() {
-                                          _showPassword = !_showPassword;
-                                        });
-                                      })
-                              )
-                          ),
-                        ),
-                        new Container(
-                            margin: EdgeInsets.only(top: 30),
-                            child: ButtonTheme(
-                              height: 52.0,
-                              child: new RaisedButton(
-                                key: Key('SubmitButton'),
-                                color: AppColors.blueTurquoise,
-                                child: StreamBuilder(
-                                    initialData: null,
-                                    stream: this.loginBloc.resultLogin,
-                                    builder: (BuildContext context,
-                                        AsyncSnapshot<InfoUser> snapshot) {
-                                      print("------------------------");
-                                      bool shouldShowCircular = (snapshot
-                                          .connectionState ==
-                                          ConnectionState.active &&
-                                          !snapshot.hasError);
-                                      print("shouldShowCircular? > $shouldShowCircular");
-                                      print("Loading > ${snapshot.data
-                                          .toString()}");
-                                      print("connectionState > ${snapshot
-                                          .connectionState}");
-                                      print("hasError > ${snapshot.hasError}");
-                                      print("------------------------");
-                                      return (snapshot.connectionState ==
-                                          ConnectionState.active &&
-                                          !snapshot.hasError) ? Center(
-                                        child: CircularProgressIndicator(
-                                            strokeWidth: 1,
-                                            backgroundColor: Colors.white
-                                        ),
-                                      ) : Text("Login", style: TextStyle(color: Colors.white, fontSize: 16));
-                                    }
-                                ),
-                                onPressed: () {
-                                if (_formKey.currentState.validate()) {
-                                  _formKey.currentState.save();
-
-                                  String username = _data.username;
-                                  String password = _data.password;
-
-                                  InfoUser attemptedUser = InfoUser(
-                                    user: User(
-                                      username: username,
-                                      password: password
-                                    )
-                                  );
-                                  this.loginBloc.submitLogin.add(attemptedUser);
-                                }
-                                },
-                                shape: new RoundedRectangleBorder(
-                                  borderRadius: new BorderRadius.circular(4),
-                                ),
-                              ),
-                            )
-                        ),
-                        StreamBuilder(
-                            initialData: null,
-                            stream: this.loginBloc.resultLogin,
-                            builder: (BuildContext context, AsyncSnapshot<InfoUser> snapshot) {
-                              print("data > ${snapshot.data.toString()}");
-                              if (snapshot.hasError) {
-                                return new Text("${snapshot.error.toString()}", style: TextStyle(color: Colors.red, fontSize: 12, fontStyle: FontStyle.italic));
-                              }
-                              return Text("");
-                            }
-                        ),
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisSize: MainAxisSize.max,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            LinkAppButton(
-                              onPressed: () {
-                                Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) {
-                                  return PageResetPasswordBloc(
-                                  );
-                                }));
-                              },
-                              title: "Forgot your password?",
-                              style: TextStyle(
-                                color: AppColors.gray,
-                                decoration: TextDecoration.underline
-                              ),
-                            ),
-                          ],
-                        )
-                      ],
-                    )
-                ),
-              )
-          )
-      );
-  }
-  */
 }
